05d4bc2de257851747e2c82f5687fddca88fc656
Michał Capała <michaal.capala@gmail.com>
2020-01-11 03:17:34 +0100

    Final commit

--- src/main/java/pl/wat/mc/jfk/lab2/ClassesManager.java
+++ src/main/java/pl/wat/mc/jfk/lab2/ClassesManager.java
@@ -1,19 +1,15 @@
 package pl.wat.mc.jfk.lab2;
 
 import javassist.*;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Map;
 
 public class ClassesManager {
     private final JarContentManager jcm;
 
-
     public ClassesManager(JarContentManager jfm) {
         this.jcm = jfm;
-
     }
 
     public void addClass(String classPath) {
@@ -30,70 +26,61 @@ public class ClassesManager {
         CtClass newInterface = jcm.getJarClassPool().makeInterface(interfacePath);
         jcm.saveCtClass(newInterface);
     }
+
     public void removeInterface(String interfacePath) {
         CtClass newInterface = jcm.getJarClassPool().makeInterface(interfacePath);
         jcm.saveCtClass(newInterface);
     }
+
     public void addPackage(String packagePath){
         jcm.createFolder(packagePath.replaceAll("\\.","/"));
     }
+
     public void removePackage(String packagePath){
         jcm.deleteFile(packagePath.replaceAll("\\.","/"));
     }
 
-
-    public void addMethod(String classPath, String accesibility, boolean isStatic, String returnType, String methodName, Map<String, String> arguments, String body) throws CannotCompileException {
-        StringBuilder methodBodyBuilder = new StringBuilder();
-        CtClass clazz = getCtClass(classPath);
-        CtMethod newMethod;
-
-
-        methodBodyBuilder.append(accesibility).append(" ");
-        if (isStatic) methodBodyBuilder.append("static ");
-        methodBodyBuilder.append(returnType).append(" ").append(methodName);
-        methodBodyBuilder.append(getArgumentsAsString(arguments));
-        methodBodyBuilder.append(body);
-        newMethod =  CtNewMethod.make(methodBodyBuilder.toString(), clazz);
-        clazz.addMethod(newMethod);
-        jcm.saveCtClass(clazz);
-
-
+    public void addMethod(String classPath, String src) throws CannotCompileException {
+        CtClass declaring = getCtClass(classPath);
+        CtMethod newMethod = CtNewMethod.make(src,declaring);
+        declaring.addMethod(newMethod);
+        jcm.saveCtClass(declaring);
     }
 
-    public void removeMethod(String classPath, String methodName) throws NotFoundException {
+    public void removeMethod(String classPath, String methodName,CtClass [] params) throws NotFoundException {
         CtClass cc = getCtClass(classPath);
-        CtMethod ctm = getCtMethod(cc, methodName);
+        CtMethod ctm = getCtMethod(cc, methodName,params);
         cc.removeMethod(ctm);
         jcm.saveCtClass(cc);
     }
 
-    public void addCodeBeforeMethod(String classPath, String methodName, String methodBody) throws CannotCompileException {
+    public void addCodeBeforeMethod(String classPath, String methodName,CtClass [] params, String methodBody) throws CannotCompileException {
         CtClass cc = getCtClass(classPath);
-        CtMethod ctm = getCtMethod(cc, methodName);
+        CtMethod ctm = getCtMethod(cc, methodName,params);
         ctm.insertBefore(methodBody);
         jcm.saveCtClass(cc);
 
     }
 
-    public void addCodeAfterMethod(String classPath, String methodName, String methodBody) throws CannotCompileException {
+    public void addCodeAfterMethod(String classPath, String methodName,CtClass [] params, String methodBody) throws CannotCompileException {
         CtClass cc = getCtClass(classPath);
-        CtMethod ctm = getCtMethod(cc, methodName);
+        CtMethod ctm;
+        ctm = getCtMethod(cc, methodName,params);
         ctm.insertAfter(methodBody);
         jcm.saveCtClass(cc);
     }
 
-    public void setMethodBody(String classPath, String methodName, String methodBody) throws CannotCompileException {
+
+    public void setMethodBody(String classPath, String methodName,CtClass [] params, String methodBody) throws CannotCompileException {
         CtClass cc = getCtClass(classPath);
-        CtMethod ctm = getCtMethod(cc, methodName);
+        CtMethod ctm = getCtMethod(cc, methodName,params);
         ctm.setBody(methodBody);
         jcm.saveCtClass(cc);
     }
 
-    public void addField(String classPath, String accessibility, String type, String fieldName) throws CannotCompileException {
+    public void addField(String classPath, String src) throws CannotCompileException {
         CtClass cc = getCtClass(classPath);
-        StringBuilder fieldBuilder = new StringBuilder();
-        fieldBuilder.append(accessibility).append(" ").append(type).append(" ").append(fieldName).append(";");
-        CtField newField = CtField.make(fieldBuilder.toString(), cc);
+        CtField newField = CtField.make(src, cc);
         cc.addField(newField);
         jcm.saveCtClass(cc);
     }
@@ -105,84 +92,70 @@ public class ClassesManager {
         jcm.saveCtClass(cc);
     }
 
-    public void addConstructor(String classPath, String accessibility, Map<String, String> arguments,String body) throws CannotCompileException {
-       // System.out.println("dodawanie konstruktora");
+    public void addConstructor(String classPath, String src) throws CannotCompileException {
         CtClass cc = getCtClass(classPath);
-        StringBuilder constructorBuilder = new StringBuilder();
-        constructorBuilder.append(accessibility).append(" ").append(cc.getSimpleName()).append(" ").append(getArgumentsAsString(arguments)).append(body);
-        System.out.println(constructorBuilder.toString());
-        CtConstructor con = CtNewConstructor.make(constructorBuilder.toString(), cc);
+        CtConstructor con = CtNewConstructor.make(src, cc);
         cc.addConstructor(con);
         jcm.saveCtClass(cc);
     }
 
-    public void removeConstructor(String classPath, Map<String, String> arguments) throws NotFoundException {
+    public void removeConstructor(String classPath, CtClass[] arguments) throws NotFoundException {
         CtClass cc = getCtClass(classPath);
-        CtConstructor[] cons = cc.getConstructors();
-        System.out.println("masno"+cons[0].getParameterTypes()[0].getName());
-        CtConstructor con = cc.getConstructor(getArgumentsAsString(arguments));
-
-
+        CtConstructor con = cc.getDeclaredConstructor(arguments);
         cc.removeConstructor(con);
         jcm.saveCtClass(cc);
     }
 
-    public void setConstructorBody(String classPath, Map<String, String> arguments, String constructorBody) throws NotFoundException, CannotCompileException {
+    public void setConstructorBody(String classPath, CtClass[] arguments, String constructorBody) throws NotFoundException, CannotCompileException {
         CtClass cc = getCtClass(classPath);
-        CtConstructor con = cc.getConstructor(getArgumentsAsString(arguments));
+        CtConstructor con = cc.getDeclaredConstructor(arguments);
         con.setBody(constructorBody);
         jcm.saveCtClass(cc);
     }
 
-    public void listClassMethods(String classPath) throws CannotCompileException {
-        Class clazz = getCtClass(classPath).toClass();
+    public void listClassMethods(String classPath) {
+        Class<?> clazz = jcm.loadClass(classPath);
         Method[] methods = clazz.getDeclaredMethods();
         System.out.println("Methods in class " + classPath);
-        for (Method m : methods) {
-            System.out.println(m.getName());
+       for (Method m : methods) {
+           System.out.println(m.toString());
+         }
+    }
+
+    public void listClassFields(String classPath) {
+        Class<?> clazz = jcm.loadClass(classPath);
+        if(clazz!=null) {
+            Field[] fields = clazz.getDeclaredFields();
+            System.out.println("Fields in class " + classPath);
+            for (Field f : fields) {
+                System.out.println(f.toString());
+            }
+
         }
     }
 
-    public void listClassFields(String classPath) throws CannotCompileException {
-        Class clazz = getCtClass(classPath).toClass();
-        Field[] fields = clazz.getDeclaredFields();
-        System.out.println("Fields in class " + classPath);
-        for (Field f : fields) {
-            System.out.println(f.getName());
+    public void listClassConstructors(String classPath) {
+        Class<?> clazz = jcm.loadClass(classPath);
+        if(clazz!=null) {
+            Constructor<?>[] constructors = clazz.getConstructors();
+            System.out.println("Constructors in class " + classPath);
+            for (Constructor<?> c : constructors) {
+                System.out.println(c.toString());
+            }
         }
     }
 
-    public void listClassConstructors(String classPath) throws CannotCompileException {
-        Class clazz = getCtClass(classPath).toClass();
-        Constructor[] constructors = clazz.getConstructors();
-        System.out.println("Fields in class " + classPath);
-        for (Constructor c : constructors) {
-            System.out.println(c.getName());
-        }
-    }
 
-    public String getArgumentsAsString(Map<String, String> arguments) {
-        StringBuilder argumentsBuilder = new StringBuilder();
-        argumentsBuilder.append("(");
-
-        for (Map.Entry<String, String> entry : arguments.entrySet()) {
-            argumentsBuilder.append(entry.getKey()).append(" ").append(entry.getValue()).append(",");
-        }
-
-        argumentsBuilder.append(")");
-
-        //Usunięcie ostatniego
-        return argumentsBuilder.toString().replaceAll(",\\s*(?=\\))","");
-
-    }
-
-    public CtMethod getCtMethod(CtClass cc, String methodName) {
+    public CtMethod getCtMethod(CtClass cc, String methodName,CtClass [] params) {
         CtMethod cm = null;
         try {
-            for(CtMethod cm2:cc.getDeclaredMethods()){
-                System.out.println(cm2.getName());
+            if(params==null) {
+                cm = cc.getDeclaredMethod(methodName);
             }
-            cm = cc.getDeclaredMethod(methodName);
+            else{
+                cm = cc.getDeclaredMethod(methodName,params);
+            }
+
         } catch (NotFoundException e) {
             System.out.println("Error in getting method from class" + cc.getName());
             e.printStackTrace();
@@ -190,26 +163,22 @@ public class ClassesManager {
         return cm;
     }
 
+
     public CtClass getCtClass(String classPath) {
         CtClass temp = null;
         try {
-            System.out.println(classPath);
             temp = jcm.getJarClassPool().get(classPath);
-
+            temp.defrost();
         } catch (Exception e) {
             System.out.println("Error in getting class from class Pool");
         }
         if(temp==null){
-            try {
-                throw new Exception("Class "+classPath+" doesnt exist");
-            } catch (Exception e) {
-                e.printStackTrace();
-                jcm.saveAndExit();
-                System.exit(0);
-            }
+            System.out.println("Class "+classPath+" does not exist.");
         }
-        temp.defrost();
+
         return temp;
 
     }
+
+
 }
