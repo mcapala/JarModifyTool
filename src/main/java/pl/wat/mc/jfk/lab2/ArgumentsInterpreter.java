ceb090b1152f2771c04388e2e6124ac567a60da2
Michał Capała <michaal.capala@gmail.com>
2020-01-12 22:21:57 +0100

    Added arguments verification

--- src/main/java/pl/wat/mc/jfk/lab2/ArgumentsInterpreter.java
+++ src/main/java/pl/wat/mc/jfk/lab2/ArgumentsInterpreter.java
@@ -4,6 +4,7 @@ import javassist.CannotCompileException;
 import javassist.NotFoundException;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 
 public class ArgumentsInterpreter {
 
@@ -12,21 +13,27 @@ public class ArgumentsInterpreter {
     private ClassEditor cm;
     private ScriptInterpreter si;
     public ArgumentsInterpreter(String[] args) throws IOException, NotFoundException, CannotCompileException {
+        if(args.length<2) new IllegalArgumentException("Invalid number of arguments.");
         this.args = args;
         mainLoop();
     }
     public void mainLoop() throws IOException, NotFoundException, CannotCompileException {
         String operationType = null;
         boolean listingFlag = false;
+        boolean inputSet = false;
         boolean scriptFlag = false;
         boolean classPathFlag = false;
+        boolean listClassesFlag = false;
+        boolean listPackagesFlag = false;
         for(String arg: args){
             if (arg.equals("--list-classes")) {
                 listingFlag = true;
-                jcm.printClassNames();
+                listClassesFlag=true;
+
             } else if (arg.equals("--list-packages")) {
                 listingFlag = true;
-                jcm.printPackageNames();
+                listPackagesFlag = true;
+
             }
             else if(arg.charAt(0)=='-'&&arg.charAt(1)=='-') operationType = arg;
             else {
@@ -34,6 +41,7 @@ public class ArgumentsInterpreter {
                 if(operationType.equals("--i")){
                     jcm = new JarContentManager(arg);
                     cm = new ClassEditor(jcm);
+                    inputSet = true;
                 }
                 else if(operationType.equals("--o")){
                     jcm.outputFileName = arg;
@@ -41,9 +49,8 @@ public class ArgumentsInterpreter {
                 else if(operationType.equals("--script")){
                     if(!listingFlag) {
                         scriptFlag=true;
-                        jcm.initForScript();
                         si = new ScriptInterpreter(arg, jcm, cm);
-                    }
+                    }else throw new IllegalArgumentException();
                 }
                 else if(operationType.equals("--cp")){
                     jcm.addedClassPath = arg;
@@ -65,16 +72,35 @@ public class ArgumentsInterpreter {
                         operationType ="";
                     }
                 }
+
             }
         }
+        if(!inputSet){
+            throw new IllegalArgumentException("Input file not set.");
+        }
 
-        if(classPathFlag)jcm.addClassPath();
+        if(classPathFlag){
+            if(listPackagesFlag|listClassesFlag) throw new IllegalArgumentException("Invalid arguments.");
+            jcm.addClassPath();
+        }
 
         if(scriptFlag){
-            if(jcm.outputFileName==null){
-                System.out.println("Output file name is not set.");
+            if(listPackagesFlag|listClassesFlag|listingFlag) throw new IllegalArgumentException("Invalid arguments.");
+            if(jcm.outputFileName==""){
+                throw new NoSuchFileException("Output file name is not set.");
             }
-            else si.fileInterpreter();
+            else{
+                jcm.initForScript();
+                si.fileInterpreter();
+            }
+        }
+        if(listClassesFlag){
+            if(scriptFlag|classPathFlag) throw new IllegalArgumentException("Invalid arguments.");
+            jcm.printClassNames();
+        }
+        if(listPackagesFlag){
+            if(scriptFlag|classPathFlag) throw  new IllegalArgumentException("Invalid arguments.");
+            jcm.printPackageNames();
         }
     }
 }
